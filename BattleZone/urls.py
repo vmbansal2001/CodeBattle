f7de4383c9174b93659e798bfc712f92a6c1a127
Vipul Bansal <vmbansal2001@gmail.com>
2021-11-30 21:43:25 +0530

    Favicon added

--- BattleZone/urls.py
+++ BattleZone/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from BattleZone import views
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 urlpatterns = [
     path('', views.index, name='home'), #Home Page
@@ -30,4 +32,7 @@ urlpatterns = [
     path('about', views.about, name='about'), #About Page
     path('ide', views.ide, name='ide'), #IDE
     path('executeCode', views.executeCode, name='executeCode'), #To execute IDE code
+    path('favicon.ico',
+        RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),
+    ),
 ]
